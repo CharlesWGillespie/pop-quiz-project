50447b148b8b79b06f9c497575b96d8e288650c4
CharlesWGillespie <charlesgillespiew@gmail.com>
2023-10-06 18:28:20 -0400

    added initial question to webpage

--- assets/script.js
+++ assets/script.js
@@ -22,4 +22,28 @@ document.addEventListener("DOMContentLoaded", function (){
     const initialTime = 60;
     startTimer(initialTime, timerDisplay)
 })
-    
+
+document.addEventListener("DOMContentLoaded", function () {
+    const quizForm = document.getElementById("quiz-form");
+
+    quizForm.addEventListener("submit", function (e) {
+        e.preventDefault(); // Prevent the form from submitting
+
+        // Get the selected answer
+        const selectedAnswer = document.querySelector('input[name="answer"]:checked');
+
+        if (!selectedAnswer) {
+            alert("Please select an answer."); // Display an alert if no answer is selected
+            return;
+        }
+
+        const userAnswer = selectedAnswer.value;
+
+        // Check the user's answer (you can add your logic here)
+        if (userAnswer === "A") {
+            alert("Correct!"); // Display a message for a correct answer
+        } else {
+            alert("Incorrect. Try again!"); // Display a message for an incorrect answer
+        }
+    });
+});
